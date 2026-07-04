4494b7085a00c86deec6c58a8fbdec95971530ea
conectionist <conectionist@users.noreply.github.com>
2026-07-05 00:27:28 +0300

    Add: meeting-schedule - submission-2

---
+++ Data Structures & Algorithms/meeting-schedule/submission-2.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    bool canAttendMeetings(vector<Interval>& intervals) {
+        if(intervals.size() == 0)
+            return true;
+
+        auto comp = [](const Interval& i1, const Interval& i2)
+        {
+            return i1.start < i2.start;
+        };
+
+        std::sort(intervals.begin(), intervals.end(), comp);
+
+        for(int i = 0 ; i < intervals.size() - 1 ; i++)
+        {
+            if(intervals[i].end > intervals[i+1].start)
+                return false;
+        }
+
+        return true;
+    }
+};
