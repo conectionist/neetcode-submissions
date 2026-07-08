74a52b165386c7eac2a57ab59f85d43da5aa9b44
conectionist <conectionist@users.noreply.github.com>
2026-07-08 16:17:08 +0300

    Add: islands-and-treasure - submission-1

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-1.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    void islandsAndTreasure(vector<vector<int>>& grid) {
+        queue<pair<int, int>> q;
+        int m = grid.size();
+        int n = grid[0].size();
+
+        for(int i = 0 ; i < m ; i++)
+            for(int j = 0 ; j < n ; j++)
+            {
+                if(grid[i][j] == 0)
+                {
+                    q.push({i,j});
+                }
+            }
+
+        vector<pair<int, int>> dirs = {{0,1},{0,-1},{1,0},{-1,0}};
+
+        while(!q.empty())
+        {
+            auto [ci, cj] = q.front();
+            q.pop();
+
+            for(auto& [di, dj] : dirs)
+            {
+                int i = ci + di;
+                int j = cj + dj;
+
+                if(i >= 0 && j >= 0 && i < m && j < n && grid[i][j] == INT_MAX)
+                {
+                    grid[i][j] = grid[ci][cj] + 1;
+                    q.push({i,j});
+                }
+            }
+        }
+    }
+};
