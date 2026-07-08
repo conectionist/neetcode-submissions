43de14767f297f03a7ada5764a1e8a6731549150
conectionist <conectionist@users.noreply.github.com>
2026-07-08 19:58:28 +0300

    Add: course-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-0.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    vector<int> topoSort(vector<vector<int>> adj)
+    {
+        int n = adj.size();
+        vector<int> inDegree(n, 0);
+
+        for(int i = 0 ; i < n ; i++)
+            for(const auto neighbor : adj[i])
+                inDegree[neighbor]++;
+
+        queue<int> q;
+
+        for(int i = 0 ; i < n ; i++)
+            if(inDegree[i] == 0)
+                q.push(i);
+
+        vector<int> topoSortedList;
+
+        while(!q.empty())
+        {
+            auto current = q.front();
+            q.pop();
+            topoSortedList.push_back(current);
+
+            for(const auto neighbor : adj[current])
+            {
+                inDegree[neighbor]--;
+
+                if(inDegree[neighbor] == 0)
+                    q.push(neighbor);
+            }
+        }
+
+        return topoSortedList;
+    }
+
+    vector<vector<int>> buildAdjancencyList(int n, vector<vector<int>>& prerequisites)
+    {
+        vector<vector<int>> adj(n);
+
+        for(auto& prereq : prerequisites)
+            adj[prereq[1]].push_back(prereq[0]);
+        
+        return adj;
+    }
+
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        auto topoSortedList = topoSort(buildAdjancencyList(numCourses, prerequisites));
+
+        return topoSortedList.size() == numCourses ? topoSortedList : vector<int>();
+    }
+};
